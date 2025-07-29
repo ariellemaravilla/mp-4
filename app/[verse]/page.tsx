7ca5153f9cce36cef20f324294e3d4e52a79b8f8
arielle maravilla <amaravil@bu.edu>
2025-07-28 20:35:28 -0400

    committing

---
+++ app/[verse]/page.tsx
@@ -0,0 +1,15 @@
+import getVerse from "@/lib/getVerse";
+import type { BibleVerse } from "@/types";
+
+export default async function VersePage({ params }: { params: { verse: string } }) {
+    const verse: BibleVerse | undefined | null = await getVerse(params.verse);
+
+    if (!verse) return <p style={{ textAlign: "center" }}>Verse not found or API limit reached.</p>;
+
+    return (
+        <main style={{ padding: "2rem", maxWidth: "700px", margin: "0 auto" }}>
+            <h1 style={{ fontSize: "2rem", textAlign: "center" }}>{verse.reference}</h1>
+            <p style={{ fontSize: "1.25rem", lineHeight: "1.6" }} dangerouslySetInnerHTML={{ __html: verse.content }} />
+        </main>
+    );
+}
