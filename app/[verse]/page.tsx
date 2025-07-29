b695181bcfedbff6a13adc040087f123932d24ff
arielle maravilla <amaravil@bu.edu>
2025-07-28 21:11:07 -0400

    vercel edit

--- app/[verse]/page.tsx
+++ app/[verse]/page.tsx
@@ -1,15 +1,31 @@
 import getVerse from "@/lib/getVerse";
 import type { BibleVerse } from "@/types";
+import type { JSX } from "react";
 
-export default async function Page({ params }: { params: { verse: string } }) {
+
+type PageProps = {
+    params: {
+        verse: string;
+    };
+};
+
+export default async function VersePage({
+                                            params,
+                                        }: PageProps): Promise<JSX.Element> {
     const verse: BibleVerse | undefined | null = await getVerse(params.verse);
 
     if (!verse)
-        return <p style={{ textAlign: "center" }}>Verse not found or API limit reached.</p>;
+        return (
+            <p style={{ textAlign: "center" }}>
+                Verse not found or API limit reached.
+            </p>
+        );
 
     return (
         <main style={{ padding: "2rem", maxWidth: "700px", margin: "0 auto" }}>
-            <h1 style={{ fontSize: "2rem", textAlign: "center" }}>{verse.reference}</h1>
+            <h1 style={{ fontSize: "2rem", textAlign: "center" }}>
+                {verse.reference}
+            </h1>
             <p
                 style={{ fontSize: "1.25rem", lineHeight: "1.6" }}
                 dangerouslySetInnerHTML={{ __html: verse.content }}
